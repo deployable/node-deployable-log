62e9f5f31adb0e66aa2524e348834bc3c91fb206
Matt Hoyle <code@deployable.co>
2017-03-06 11:52:51 +0000

    test log stream formatting and variables names

--- test/unit_log_spec.js
+++ test/unit_log_spec.js
@@ -32,16 +32,16 @@ describe('Unit::Deployable::log::log', function(){
     })
 
     it('log entries are passed to stream', function(){
-      let me = Log.fetch('me')
-      me.logger.info('testa info')
-      me.logger.level = 'debug'
-      me.logger.debug('test debug')
-      me.logger.level = 'trace'
-      me.logger.trace('test trace')
-      me.logger.info('test info2')
-      me.logger.trace('test trace')
+      let melog = Log.fetch('me')
+      melog.logger.info('testa info')
+      melog.logger.level = 'debug'
+      melog.logger.debug('test debug')
+      melog.logger.level = 'trace'
+      melog.logger.trace('test trace')
+      melog.logger.info('test info2')
+      melog.logger.trace('test trace')
       let logdata = Log.testGetLastLog()
-      expect( logdata ).to.contain.keys('msg','time','_logger','level')
+      expect( logdata ).to.contain.keys( 'msg','time','_logger','level' )
       debug('logdata', logdata)
       expect( logdata.msg ).to.equal( 'test trace' )
       expect( logdata.time ).to.be.a( 'number' )
