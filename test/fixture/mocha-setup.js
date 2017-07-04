621e198e42edf109cae5e9cc04977b6760766cf7
Matt Hoyle <code@deployable.co>
2017-07-04 02:38:28 +0000

    remove bluebird setup from test setup

--- test/fixture/mocha-setup.js
+++ test/fixture/mocha-setup.js
@@ -2,9 +2,4 @@ global.chai = require('chai')
 global.sinon = require('sinon')
 global.expect = require('chai').expect
 
-require('bluebird').config({
-  longStackTraces: true,
-  warnings: true
-})
-
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'test'
